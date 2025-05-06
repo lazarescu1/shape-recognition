50a23453f8d852861d2d0639ae9058ca56eca03a
lazarescu1 <lazarescu.sebi1@gmail.com>
2025-05-06 11:56:11 +0300

    some progress + added the homework info pdf

--- main.m
+++ main.m
@@ -3,16 +3,21 @@
 
 clc
 close all
+
+% reading the original image
 img = imread("images/shapes.png");
 figure, imshow(img)
 
+% making the rgb image gray
 gray = rgb2gray(img);
 figure, imshow(gray)
 
+% binarizing the image
 T = graythresh(gray);
 bw = imbinarize(gray, T);
 figure, imshow(bw)
 
-se = strel("arbitrary", 5);
-background = imopen(bw, se);
-% figure, imshow(background)
+% getting the edges of the geometrical shapes
+% with the Sobel filter
+edges = edge(bw, 'sobel');
+figure, imshow(edges)
