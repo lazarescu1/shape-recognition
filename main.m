2e2323394f0f22c19a34f78fbec859c85356d754
lazarescu1 <lazarescu.sebi1@gmail.com>
2025-05-06 11:29:31 +0300

    minor changes

--- main.m
+++ main.m
@@ -3,16 +3,16 @@
 
 clc
 close all
-img = imread("shapes.png");
-% figure, imshow(img)
+img = imread("images/shapes.png");
+figure, imshow(img)
 
 gray = rgb2gray(img);
-% figure, imshow(gray)
+figure, imshow(gray)
 
 T = graythresh(gray);
 bw = imbinarize(gray, T);
-% figure, imshow(bw)
+figure, imshow(bw)
 
 se = strel("arbitrary", 5);
 background = imopen(bw, se);
-figure, imshow(background)
+% figure, imshow(background)
